730a536c9a424ffeed177f6b9b70df3b405c84f2
chenjianyu <Cjy547856>
2018-03-28 17:23:55 +0800

    增加缓存工具类包

---
+++ gts-manage/src/main/java/com/wf/gts/manage/constant/Constants.java
@@ -0,0 +1,14 @@
+package com.wf.gts.manage.constant;
+
+public interface Constants {
+  
+    public static final int CONSTANT_INT_ZERO = 0;
+
+    String applicationName = "gts-manager";
+
+    String REDIS_PRE_FIX = "transaction:group:%s";
+
+    String REDIS_KEYS = "transaction:group:*";
+
+
+}
