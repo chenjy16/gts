d447a778d678ca24cd2f61600a507a840fbfb16a
chenjianyu <Cjy547856>
2018-03-28 16:42:05 +0800

    增加配置类

---
+++ gts-nameserver/src/main/java/com/wf/gts/nameserver/NameSrvApplication.java
@@ -0,0 +1,15 @@
+package com.wf.gts.nameserver;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class NameSrvApplication {
+  
+  public static void main(String[] args) {
+      SpringApplication.run(NameSrvApplication.class, args);
+  }
+  
+}
